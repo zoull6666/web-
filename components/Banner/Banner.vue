58e33a55cc78017d434d4ddb1fb043f513f32d71
doubleZoululu <15779218740@163.com>
2022-11-02 14:35:46 +0800

    8888

--- components/Banner/Banner.vue
+++ components/Banner/Banner.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="banner" :style="{'background-image':`url(${bannerData.img})`}">
+        <p>aassa</p>
         <div class="text_box">
             <p>{{bannerData.text}}888888</p>
             <span :class='{"bg":bannerData.bgcolor}'>{{bannerData.title}}</span>
