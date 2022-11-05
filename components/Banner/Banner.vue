101890f353235583484b052695332e6786670389
zoull6666 <13535398514@163.com>
2022-11-05 17:33:24 +0800

    回滚代码1.0

--- components/Banner/Banner.vue
+++ components/Banner/Banner.vue
@@ -2,10 +2,9 @@
     <div class="banner" :style="{'background-image':`url(${bannerData.img})`}">
         <p>aassa</p>
         <div class="text_box">
-            <p>{{bannerData.text}}adsadaasdass</p>
-            <span :class='{"bg":bannerData.bgcolor}'>{{bannerData.title}}</span>
+           <p>{{bannerData.text}}1111333</p>
+            <span :class='{"bg":bannerData.bgcolor}'>{{bannerData.title}}2222</span>
         </div>
-        <p>asdasd</p>
         <slot name='label'></slot>
     </div>
 </template>
--- components/content/content.vue
+++ components/content/content.vue
@@ -6,7 +6,7 @@
       :class="{content : isOpen == true, contentAll : isOpen == false,maxHeight: isHeight == true}"
       style="-webkit-box-orient: vertical;"
     ></div>
-    <span v-show="isShow" @click="toChange()">{{isOpen ? '全文': '收起'}}sdfsdfsf</span>
+    <span v-show="isShow" @click="toChange()">{{isOpen ? '全文': '收起'}}</span>
   </div>
 
 </template>
